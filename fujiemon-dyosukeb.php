679a131b56e4bf93ef43aca0cf8df0ff082997fc
Kentarou Fujie <fujiken1235@Ken-mac.local>
2012-08-02 02:00:17 +0900

    commit

--- fujiemon-dyosukeb.php
+++ fujiemon-dyosukeb.php
@@ -1,7 +1,19 @@
 <div id="fy-1" class="step test" data-x="-5000" data-y="1500" data-scale="2">
-  <p>This is sample for aa.</p>
-  <ul><li>hoge</li><li>hoge</li></ul>
+<h2>github APIでissues trackingと</h2>
+</h2>　　　　　　markdown レンダリング</h2>
+<br>
+<br>
+<ul>
+<li>・githubのAPIを利用</li>
+<li>・issuesの閲覧管理</li>
+<li>・README.mdの閲覧</li>
+</ul>
 </div>
+
 <div id="fy-2" class="step test" data-x="-3000" data-y="1500" data-scale="2">
-  <p>This is 2nd sample for aa.</p>
+<h3>実装</h3>
+<ul>
+<li>言語：php</li>
+</ul>
+<p>APIから情報を取得し表示したり、得られた情報の解析など。実装中…</p>
 </div>
